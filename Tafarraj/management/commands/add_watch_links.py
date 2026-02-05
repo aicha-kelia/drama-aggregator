84ed8ff5c8d855a06894da67b1479668e01c4918
Aicha <aichakelia01@gmail.com>
2026-02-05 20:52:24 +0100

    Added watch links script

--- Tafarraj/management/commands/add_watch_links.py
+++ Tafarraj/management/commands/add_watch_links.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from Tafarraj.models import Drama, WatchLink
 
 class Command(BaseCommand):
-    help = 'Add reliable watch links for dramas'
+    help = 'Add VERIFIED watch links with Arabic subtitles for dramas'
 
     def handle(self, *args, **options):
         # Clear old links
@@ -16,59 +16,68 @@ class Command(BaseCommand):
             title_dash = drama.title.lower().replace(' ', '-')
             title_ar = drama.title_arabic.replace(' ', '+')
             
-            # KOREAN DRAMAS - Most reliable sources
+            # KOREAN DRAMAS - Sites with Arabic subs
             if drama.country == 'korean':
                 links = [
-                    # International platforms (most reliable)
-                    {'name': 'Viki', 'url': f'https://www.viki.com/search?q={title}'},
-                    {'name': 'Netflix', 'url': f'https://www.netflix.com/search?q={title}'},
-                    {'name': 'MyDramaList', 'url': f'https://mydramalist.com/search?q={title}'},
-                    
-                    # Arabic platforms
-                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title}'},
+                    # FREE Arabic sites (VERIFIED)
+                    {'name': 'Aradrama', 'url': f'https://aradramatv.com/?s={title}'},
+                    {'name': 'Asia2TV', 'url': f'https://asia2tv.com/?s={title}'},
+                    {'name': 'Best-Drama', 'url': f'https://best-drama.com/?s={title}'},
+                    {'name': 'ArabSeed', 'url': f'https://arabseed.show/?s={title_ar}'},
+                    {'name': 'Drama Slayer', 'url': f'https://dramaslayer.net/?s={title}'},
                     {'name': 'Akwam', 'url': f'https://akwam.to/search?q={title_ar}'},
-                    {'name': 'Faselhd', 'url': f'https://faselhd.io/?s={title_ar}'},
+                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title}'},
+                    
+                    # Official platforms
+                    {'name': 'Netflix', 'url': f'https://www.netflix.com/search?q={title}'},
+                    {'name': 'Viki', 'url': f'https://www.viki.com/search?q={title}'},
                 ]
             
-            # TURKISH DRAMAS
+            # TURKISH DRAMAS - Sites with Arabic subs/dub
             elif drama.country == 'turkish':
                 links = [
-                    # Turkish drama specialists
+                    # FREE Arabic sites (VERIFIED)
                     {'name': 'قصة عشق', 'url': f'https://3isk.cam/?s={title}'},
-                    {'name': 'قصة عشق الاصلي', 'url': f'https://www.3sk.tv/?s={title}'},
-                    
-                    # Arabic platforms
-                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title_ar}'},
                     {'name': 'Akwam', 'url': f'https://akwam.to/search?q={title_ar}'},
                     {'name': 'MyCima', 'url': f'https://mycima.tv/search/{title_dash}'},
-                    {'name': 'Egybest', 'url': f'https://egybest.deals/explore/?q={title}'},
+                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title_ar}'},
+                    {'name': 'ArabSeed', 'url': f'https://arabseed.show/?s={title_ar}'},
+                    {'name': 'EgyBest', 'url': f'https://egybest.deals/explore/?q={title}'},
+                    
+                    # Official platforms
+                    {'name': 'Netflix', 'url': f'https://www.netflix.com/search?q={title}'},
+                    {'name': 'StarzPlay', 'url': f'https://starzplay.com/en/search?q={title}'},
                 ]
             
-            # CHINESE DRAMAS
+            # CHINESE DRAMAS - Sites with Arabic subs
             elif drama.country == 'chinese':
                 links = [
-                    # International platforms
-                    {'name': 'Viki', 'url': f'https://www.viki.com/search?q={title}'},
+                    # FREE Arabic sites (VERIFIED)
+                    {'name': 'Aradrama', 'url': f'https://aradramatv.com/?s={title}'},
+                    {'name': 'Best-Drama', 'url': f'https://best-drama.com/?s={title}'},
+                    {'name': 'Asia2TV', 'url': f'https://asia2tv.com/?s={title}'},
+                    {'name': 'Akwam', 'url': f'https://akwam.to/search?q={title_ar}'},
+                    {'name': 'ArabSeed', 'url': f'https://arabseed.show/?s={title_ar}'},
+                    
+                    # Official platforms
                     {'name': 'WeTV', 'url': f'https://wetv.vip/en/search?q={title}'},
                     {'name': 'iQIYI', 'url': f'https://www.iq.com/search/{title}'},
                     {'name': 'Netflix', 'url': f'https://www.netflix.com/search?q={title}'},
-                    
-                    # Arabic platforms
-                    {'name': 'Akwam', 'url': f'https://akwam.to/search?q={title_ar}'},
-                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title_ar}'},
                 ]
             
-            # INDIAN DRAMAS
-            elif drama.country == 'indian':
+            # MOROCCAN DRAMAS - Already in Arabic
+            elif drama.country == 'moroccan':
                 links = [
-                    # Indian platforms
-                    {'name': 'Hotstar', 'url': f'https://www.hotstar.com/in/search?q={title}'},
-                    {'name': 'Zee5', 'url': f'https://www.zee5.com/search?q={title}'},
+                    # Official platforms
+                    {'name': 'Shahid VIP', 'url': f'https://shahid.mbc.net/ar/search?q={title_ar}'},
+                    {'name': 'Forja', 'url': f'https://forja.ma/search?q={title_ar}'},
                     
-                    # Arabic platforms
-                    {'name': 'Shahid', 'url': f'https://shahid.mbc.net/ar/search?q={title_ar}'},
+                    # FREE Arabic sites
                     {'name': 'Akwam', 'url': f'https://akwam.to/search?q={title_ar}'},
                     {'name': 'MyCima', 'url': f'https://mycima.tv/search/{title_dash}'},
+                    {'name': 'ArabSeed', 'url': f'https://arabseed.show/?s={title_ar}'},
+                    {'name': 'TvFHD', 'url': f'https://av.tvfun.me/?s={title_ar}'},
+                    {'name': 'Mosalsalat', 'url': f'https://web.mosalsalat.net/?s={title_ar}'},
                 ]
             
             else:
@@ -84,6 +93,6 @@ class Command(BaseCommand):
                     episodes_available=drama.total_episodes
                 )
             
-            self.stdout.write(f'✓ {drama.title_arabic} ({drama.country})')
+            self.stdout.write(f'✓ {drama.title_arabic} ({drama.country}) - {len(links)} links')
         
-        self.stdout.write(self.style.SUCCESS(f'✅ Done! Added {WatchLink.objects.count()} watch links'))
+        self.stdout.write(self.style.SUCCESS(f'\n✅ Done! Added {WatchLink.objects.count()} verified Arabic watch links'))
